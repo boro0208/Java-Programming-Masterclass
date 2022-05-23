9c6b4252564f2a2e94d2e792fb4d1d16e32e2d70
boro0208 <bororadojcic1991@gmail.com>
2022-05-23 17:53:01 +0200

    OOP tasks for encapsulation (simulate printer)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.bedroom.*;
+import com.company.printer.Printer;
 import com.company.rectanglecuboid.Cuboid;
 import com.company.rectanglecuboid.Rectangle;
 
@@ -114,16 +115,25 @@ public class Main {
 //        System.out.println("cuboid.height= " + cuboid.getHeight());
 //        System.out.println("cuboid.volume= " + cuboid.getVolume());
 
-        Wall wall1 = new Wall( "West ");
-        Wall wall2 = new Wall("East") ;
-        Wall wall3 = new Wall ( "South " );
-        Wall wall4 = new Wall("North") ;
-        Ceiling ceiling = new Ceiling (12, 55);
-        Bed bed = new Bed ("Modern", 4, 3, 2, 1);
-        Lamp lamp = new Lamp("Classic", false, 75);
-        Bedroom bedRoom = new Bedroom("YOUR NAME HERE", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
-        bedRoom.makeBed() ;
-        bedRoom.getLamp().turnOn ();
+//        Wall wall1 = new Wall( "West ");
+//        Wall wall2 = new Wall("East") ;
+//        Wall wall3 = new Wall ( "South " );
+//        Wall wall4 = new Wall("North") ;
+//        Ceiling ceiling = new Ceiling (12, 55);
+//        Bed bed = new Bed ("Modern", 4, 3, 2, 1);
+//        Lamp lamp = new Lamp("Classic", false, 75);
+//        Bedroom bedRoom = new Bedroom("YOUR NAME HERE", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+//        bedRoom.makeBed() ;
+//        bedRoom.getLamp().turnOn ();
+
+
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(50));
+        System.out.println("initial page count = " +printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was "+pagesPrinted +" new total print count for printer = "+printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was "+ pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
     }
 
 }
