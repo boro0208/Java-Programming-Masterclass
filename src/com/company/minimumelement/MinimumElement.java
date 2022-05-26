cf402313e26ac38feb6fb1de8fd2fd38b2313ef4
boro0208 <bororadojcic1991@gmail.com>
2022-05-26 10:06:49 +0200

    Arrays - task - find minimum element in array (small corection)

--- src/com/company/minimumelement/MinimumElement.java
+++ src/com/company/minimumelement/MinimumElement.java
@@ -3,16 +3,16 @@ package com.company.minimumelement;
 import java.util.Scanner;
 
 public class MinimumElement {
-
+    private static Scanner scanner = new Scanner(System.in);
     public static int readInteger(){
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
         System.out.println("How meany elements you need? \r");
         return scanner.nextInt();
     }
 
     public static int[] readElements(int number){
         int[] array = new int[number];
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
         System.out.println("Insert elements \r");
         for (int i = 0; i<number; i++){
             array[i] = scanner.nextInt();
